79e6c8b51009670a92f79460cc4b87a36229cc66
aruniverse <arun.george@uconn.edu>
2017-11-14 22:03:40 -0500

    updated code and log files

--- PA 1/hmm.py
+++ PA 1/hmm.py
@@ -10,7 +10,7 @@ import numpy as np
 import random
 
 def main():
-	lengths  = [300, 3000, 30000]#, 300000] 
+	lengths  = [300, 3000, 30000, 300000] 
 	for lIndex, lVal in enumerate(lengths):	#test for each sequence length
 		startTime = time.time()
 		vit_totalDifferences	= 0
@@ -18,9 +18,11 @@ def main():
 		totalSeqLength			= 10 * lVal
 
 		logFile = 'log'+str(lVal)+'.txt'
-		with open(logFile, 'a+') as lf :
+		with open(logFile, 'w') as lf :
 			pass	# flush the file everytime we run the tests
 		lf.close()
+
+		mleTot = np.zeros((2,6))
 
 		for i in range(0, 10):				# run ten tests for each length and report the average
 			print("running trial {}".format(i))
@@ -67,6 +69,7 @@ def main():
 			diffPosterrior	= sum(1 for a, b in zip(statePath, pPath) if a != b)
 
 			mleMat = MLE(statePath, symbolPath)
+			mleTot = np.add(mleTot, mleMat)
 
 			# the following print statements were used for testing
 			# print("Trial number {} for sequence of length {}".format(i+1, lVal))
@@ -82,26 +85,30 @@ def main():
 			# print("MLE for the Loaded die:\t{}".format(mleMat[1]))
 
 			with open(logFile, 'a+') as lf :	# append to the log file test data 10 times for each length
-				lf.write("Trial number {} for sequence of length {}".format(i+1, lVal))
-				lf.write("\nDie:   \t {}".format(statePath))
-				lf.write("\nViterbi: {}".format(vPath))
-				lf.write("\nNum of differences: {}".format(diffViterbi))
+				lf.write("\nTrial number {} for sequence of length {}".format(i+1, lVal))
+				lf.write("\nDie:   \t\t{}".format(statePath))
+				lf.write("\nViterbi: \t{}".format(vPath))
+				lf.write("\n\t\t\t Num of differences: {}".format(diffViterbi))
 				lf.write("\nPosterrior: {}".format(pPath))
-				lf.write("\nNum of differences: {}".format(diffPosterrior))
-				lf.write("\nMLE for the Fair die:\t{}".format(mleMat[0]))
-				lf.write("\nMLE for the Loaded die:\t{}".format(mleMat[1]))
+				lf.write("\n\t\t\t Num of differences: {}".format(diffPosterrior))
+				# lf.write("\nMLE for the Fair die:\t{}".format(mleMat[0]))
+				# lf.write("\nMLE for the Loaded die:\t{}".format(mleMat[1]))
 				lf.write("\n")
 			lf.close()
 			vit_totalDifferences	+= diffViterbi
 			post_totalDifferences	+= diffPosterrior
 
+		mleFin = np.divide(mleTot, 10)
+
 		with open(logFile, 'a+') as lf :
-			lf.write("\n")
+			lf.write("\nSummary for length {}".format(lVal))
 			lf.write("\nPercentage viterbi was wrong: {}".format(vit_totalDifferences/totalSeqLength))
 			lf.write("\nPercentage posterrior was wrong: {}".format(post_totalDifferences/totalSeqLength))
+			lf.write("\nMLE for the Fair die:\t{}".format(mleFin[0]))
+			lf.write("\nMLE for the Loaded die:\t{}".format(mleFin[1]))
 		lf.close()
 		endTime = time.time()
-		print("Simulation for length {}  done and can find test data in {}. Simulation took {}s".format(lVal, logFile, endTime-startTime))
+		print("Simulation for length {}  done and can find test data in {}. \nSimulation took {}s".format(lVal, logFile, endTime-startTime))
 
 def viterbi(seq, initMat, transitionMat, emissionMat):
 	numStates  = emissionMat.shape[0]
